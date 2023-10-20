ffd24f55aa3e144e9e7995b9d1bbb1daedf3d102
Gentlesoul10 <olivekelvin139@gmail.com>
2023-10-20 16:59:13 -0700

    flags and sizes defined

--- main.h
+++ main.h
@@ -5,4 +5,18 @@
 #include <unistd.h>
 int _printf(const char *format, ...);
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
 #endif
