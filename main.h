c3857bfc98bb918bc3fda8e7da991411c57eda9c
Gentlesoul10 <olivekelvin139@gmail.com>
2023-10-20 17:01:21 -0700

    Added the Struct function

--- main.h
+++ main.h
@@ -19,4 +19,29 @@ int _printf(const char *format, ...);
 #define S_LONG 2
 #define S_SHORT 1
 
+/**
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
+
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
 #endif
